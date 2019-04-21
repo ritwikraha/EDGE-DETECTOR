a89bc535071c4a3ce1b137ef71ef3694ba6180e7
Ritwik Raha <rtzdzn@gmail.com>
2019-04-21 15:51:16 +0530

    code updated

--- EDGED_DETECT.py
+++ EDGED_DETECT.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 import math
+from matplotlib import pyplot as plt
 
-img = cv2.imread('SAT.jpg',0)
+img = cv2.imread('img/testna_slika1b.bmp',0)
 kernel = np.ones((2,2),np.uint8)#using a kernel to reconstruct the image as per convenience
 
 """CLOSING OPERATION- is simply the opposite of opening operation in morphological operator
@@ -14,18 +15,16 @@ hence CLOSING= DILATION then EROSION
 OPENING=EROSION then DILATION, CLOSING is helpful in smoothening out the object)"""
 
 closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-cv2.imwrite('SAT_closing.jpg',closing)#saving the image after performing the closing operation
 dilation = cv2.dilate(closing,kernel,iterations = 1)#dilating the closed image
-cv2.imwrite('SAT_closing_dilation.jpg',dilation)
 
 """It is the difference between dilation and erosion of an image.
 The result will look like the outline of the object."""
 
 gradient = cv2.morphologyEx(dilation, cv2.MORPH_GRADIENT, kernel)
-cv2.imwrite('SAT_closing_dilation_gradient.jpg',gradient)
+
 
 edges_inv = cv2.bitwise_not(gradient)#inverting the gradient image so that the edges are better detected
-cv2.imwrite('SAT_2.jpg',edges_inv)
+
 
 """the function psnr will calculate the peak-signal-noise-ratio of two given images
 Peak Signal to Noise Ratio ( PSNR) and Mean Square Error (MSE)
@@ -51,7 +50,7 @@ from the Gaussian function"""
 def thres(imgk):
     imgbinar = cv2.adaptiveThreshold(imgk,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,11,4)
-    cv2.imwrite('SAT_1.jpg',imgbinar)
+    
     return imgbinar
 
 imgbinary= thres(edges_inv)#passing the inverted image through the thres filter
@@ -71,12 +70,20 @@ which means the adaptive threshold operation has rendered them too dissimilar
 hence they are merged in 0.3(of the thresholded image) and 0.7(of the inverted gradient) weighted addition"""
 if d<=psnr(img,gradient):
 	imgF = cv2.addWeighted(imgbinary, 0.3, edges_inv, 0.7, 0)
-	cv2.imwrite('FINAL.jpg',imgF)
+	
 
 imgF = cv2.addWeighted(imgbinary, 0.5, edges_inv, 0.5, 0)
-cv2.imwrite('FINAL.jpg',imgF)
 
 
 
 
 
+titles = ['Original Image','EDGE-INV','RAW EDGE','FINAL']
+images = [img, edges_inv, imgbinary, imgF]
+
+for i in range(4):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+
+plt.show()
